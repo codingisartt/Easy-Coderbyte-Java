3371de7d57449e9ac52d6f231718d997f470a0f9
Tubanur Balci <tubanurbalci@gmail.com>
2022-01-13 22:42:45 +0300

    Initial Commit

---
+++ Firstreverse.java
@@ -0,0 +1,15 @@
+import java.util.*; 
+
+class Firstreverse {
+
+  public static String FirstReverse(String str) {
+    // code goes here
+    return new StringBuilder(str).reverse().toString();
+  }
+
+  public static void main (String[] args) {
+    // keep this function call here
+    Scanner s = new Scanner(System.in);
+    System.out.print(FirstReverse(s.nextLine())); 
+  }
+}
